f4dd7be1fe9f437cd51de6c13a8fa84e8265c3b0
blaze <blazeyhere@gmail.com>
2025-06-22 21:56:28 +0530

    nuke ksu
    gives error

--- script.sh
+++ script.sh
@@ -18,11 +18,6 @@ echo "============="
 echo "Sync success"
 echo "============="
 
-#ksu
-cd kernel/xiaomi/mithorium-4.19 &&
-curl -LSs https://raw.githubusercontent.com/backslashxx/KernelSU/magic/kernel/setup.sh | bash - &&
-cd ../../.. &&
-
 # Export
 export BUILD_USERNAME=Blazey66
 export BUILD_HOSTNAME=crave
