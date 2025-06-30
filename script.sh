3a014e5f5168b843885e34cb93bffb351f146775
blaze <blazeyhere@gmail.com>
2025-06-30 15:46:02 +0530

    bringup axion

--- script.sh
+++ script.sh
@@ -2,13 +2,13 @@ rm -rf .repo/local_manifests/
 rm -rf prebuilts/clang/host/linux-x86
 
 #repo init
-repo init --depth=1 --no-repo-verify -u https://github.com/VoltageOS/manifest.git -b 15-qpr2 -g default,-mips,-darwin,-notdefault
+repo init --depth=1 --no-repo-verify -u https://github.com/AxionAOSP/android.git -b lineage-22.2 -g default,-mips,-darwin,-notdefault
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 #local_manifest
-git clone https://github.com/blazey66/local_manifests.git -b vos/15 .repo/local_manifests
+git clone https://github.com/blazey66/local_manifests.git -b axion/15 .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -33,4 +33,5 @@ echo "======= Export Done ======"
 echo "====== Envsetup Done ======="
 
 #build
-brunch mi439
+axion mi439 va
+ax -br
